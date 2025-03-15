ed07cb5335e0cddc22bbfddbbb160adb308ece7d
Chavindu Mannakkara <mchavindu@gmail.com>
2025-03-15 23:32:37 +0530

    Update AppointmentRepository.java

--- mentalhealth_app/src/main/java/com/example/mentalhealth_app/repositories/AppointmentRepository.java
+++ mentalhealth_app/src/main/java/com/example/mentalhealth_app/repositories/AppointmentRepository.java
@@ -1,4 +1,11 @@
 package com.example.mentalhealth_app.repositories;
 
-public class AppointmentRepository {
+import com.example.mentalhealth_app.models.Appointment;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository  // This helps Spring Boot detect it as a repository
+public interface AppointmentRepository extends MongoRepository<Appointment, String> {
+    List<Appointment> findByDoctorId(String doctorId);
 }
