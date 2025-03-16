47f213dd9ed909b79efe5dfc7564cf127961c50d
Pinsarani <pinsarani123@example.com>
2025-03-16 16:05:58 +0530

    Doctor Home Screen updated

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import DoctorLogin from "./screens/DoctorLogin";
-import DoctorHome from "./screens/DoctorHome";
-import PatientSelection from "./screens/PatientSelection";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import DoctorLogin from './screens/DoctorLogin';
+import DoctorHome from './screens/DoctorHome';
+import PatientSelection from './screens/PatientSelection';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="DoctorLogin" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator initialRouteName="DoctorLogin">
         <Stack.Screen name="DoctorLogin" component={DoctorLogin} />
         <Stack.Screen name="DoctorHome" component={DoctorHome} />
         <Stack.Screen name="PatientSelection" component={PatientSelection} />
