b57af5bd051b529297ff2bf37a426d5805f8328d
Chavindu Mannakkara <mchavindu@gmail.com>
2025-03-16 12:14:44 +0530

    Update Patien.java

--- mentalhealth_app/src/main/java/com/example/mentalhealth_app/models/Patien.java
+++ mentalhealth_app/src/main/java/com/example/mentalhealth_app/models/Patien.java
@@ -1,4 +1,38 @@
 package com.example.mentalhealth_app.models;
 
 public class Patien {
+    private String name;
+    private int age;
+    private String issue;
+
+    public Patien(String name, int age, String issue) {
+        this.name = name;
+        this.age = age;
+        this.issue = issue;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getIssue() {
+        return issue;
+    }
+
+    public void setIssue(String issue) {
+        this.issue = issue;
+    }
 }
+
