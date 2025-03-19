b3cd98928545f45e887f8e5bf1649abc69045000
ChathukaWickramarathna <chathuka.20232399@iit.ac.lk>
2025-03-19 18:16:16 +0530

    initial commit

---
+++ vibelift-mentalhealth-app-new/src/main/java/com/vibelift/vibelift_mentalhealth_app_new/VibeliftMentalhealthAppNewApplication.java
@@ -0,0 +1,13 @@
+package com.vibelift.vibelift_mentalhealth_app_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeliftMentalhealthAppNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibeliftMentalhealthAppNewApplication.class, args);
+	}
+
+}
---
+++ vibelift-mentalhealth-app-new/src/test/java/com/vibelift/vibelift_mentalhealth_app_new/VibeliftMentalhealthAppNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vibelift.vibelift_mentalhealth_app_new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeliftMentalhealthAppNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
