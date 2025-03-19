cc71cea7c61e93267fc2121dadc8ebcfe8e056ce
ChathukaWickramarathna <chathuka.20232399@iit.ac.lk>
2025-03-19 18:44:21 +0530

    feat: Add autowired dependencies for PatientRepository and PasswordEncoder

--- vibelift-mentalhealth-app-new/src/main/java/com/vibelift/vibelift_mentalhealth_app_new/controller/PatientController.java
+++ vibelift-mentalhealth-app-new/src/main/java/com/vibelift/vibelift_mentalhealth_app_new/controller/PatientController.java
@@ -1,9 +1,18 @@
 package com.vibelift.vibelift_mentalhealth_app_new.controller;
 
+import com.vibelift.vibelift_mentalhealth_app_new.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/patients")
 public class PatientController {
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 }
