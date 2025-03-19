c79c1a691fb3dcff38ef4d2e29722a13500cfdad
ChathukaWickramarathna <chathuka.20232399@iit.ac.lk>
2025-03-19 18:30:14 +0530

    feat: Add autowired dependencies for CustomUserDetailsService and PasswordEncoder

--- vibelift-mentalhealth-app-new/src/main/java/com/vibelift/vibelift_mentalhealth_app_new/config/SecurityConfig.java
+++ vibelift-mentalhealth-app-new/src/main/java/com/vibelift/vibelift_mentalhealth_app_new/config/SecurityConfig.java
@@ -1,9 +1,18 @@
 package com.vibelift.vibelift_mentalhealth_app_new.config;
 
+import com.vibelift.vibelift_mentalhealth_app_new.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 }
