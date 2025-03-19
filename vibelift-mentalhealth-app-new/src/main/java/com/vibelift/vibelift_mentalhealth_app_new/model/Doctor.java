9a96cd65feadc3856718bd08191349cb8691755b
ChathukaWickramarathna <chathuka.20232399@iit.ac.lk>
2025-03-19 18:49:07 +0530

    feat: Add Doctor class with @Document annotation

---
+++ vibelift-mentalhealth-app-new/src/main/java/com/vibelift/vibelift_mentalhealth_app_new/model/Doctor.java
@@ -0,0 +1,8 @@
+package com.vibelift.vibelift_mentalhealth_app_new.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "doctors")
+public class Doctor {
+}
