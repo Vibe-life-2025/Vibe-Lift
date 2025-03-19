1fd69aaed723a816a7b7e88316888896aa0f7e5b
ChathukaWickramarathna <chathuka.20232399@iit.ac.lk>
2025-03-19 18:50:33 +0530

    feat: Add fields (id, username, password, role) to Doctor class

--- vibelift-mentalhealth-app-new/src/main/java/com/vibelift/vibelift_mentalhealth_app_new/model/Doctor.java
+++ vibelift-mentalhealth-app-new/src/main/java/com/vibelift/vibelift_mentalhealth_app_new/model/Doctor.java
@@ -5,4 +5,10 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "doctors")
 public class Doctor {
+    @Id
+    private String id;
+
+    private String username;
+    private String password;
+    private String role; // Role field (e.g., "DOCTOR")
 }
