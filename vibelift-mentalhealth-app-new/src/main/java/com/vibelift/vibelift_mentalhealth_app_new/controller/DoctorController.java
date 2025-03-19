b6ffbcfa22f58505a163986ee1b6cffa04804e33
ChathukaWickramarathna <chathuka.20232399@iit.ac.lk>
2025-03-19 18:41:00 +0530

    feat: Add registerDoctor endpoint to handle doctor registration

--- vibelift-mentalhealth-app-new/src/main/java/com/vibelift/vibelift_mentalhealth_app_new/controller/DoctorController.java
+++ vibelift-mentalhealth-app-new/src/main/java/com/vibelift/vibelift_mentalhealth_app_new/controller/DoctorController.java
@@ -1,10 +1,11 @@
 package com.vibelift.vibelift_mentalhealth_app_new.controller;
 
+import com.vibelift.vibelift_mentalhealth_app_new.model.Doctor;
 import com.vibelift.vibelift_mentalhealth_app_new.repository.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/doctors")
@@ -15,4 +16,11 @@ public class DoctorController {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerDoctor(@RequestBody Doctor doctor) {
+        doctor.setPassword(passwordEncoder.encode(doctor.getPassword()));
+        doctorRepository.save(doctor);
+        return ResponseEntity.ok("Doctor registered successfully");
+    }
 }
