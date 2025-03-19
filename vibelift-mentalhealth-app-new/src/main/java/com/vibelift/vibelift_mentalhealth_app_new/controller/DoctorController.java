2b657b1cc1daf9a33ada0fc36365d480771b1b1e
ChathukaWickramarathna <chathuka.20232399@iit.ac.lk>
2025-03-19 18:36:30 +0530

    feat: Add DoctorController class with @RestController and @RequestMapping annotations

---
+++ vibelift-mentalhealth-app-new/src/main/java/com/vibelift/vibelift_mentalhealth_app_new/controller/DoctorController.java
@@ -0,0 +1,9 @@
+package com.vibelift.vibelift_mentalhealth_app_new.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/doctors")
+public class DoctorController {
+}
