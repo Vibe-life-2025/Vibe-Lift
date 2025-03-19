965f9f515f82a398d655f9d49c482046d82e313c
ChathukaWickramarathna <chathuka.20232399@iit.ac.lk>
2025-03-19 18:59:08 +0530

    feat: Add findByUsername method to DoctorRepository

--- vibelift-mentalhealth-app-new/src/main/java/com/vibelift/vibelift_mentalhealth_app_new/repository/DoctorRepository.java
+++ vibelift-mentalhealth-app-new/src/main/java/com/vibelift/vibelift_mentalhealth_app_new/repository/DoctorRepository.java
@@ -2,6 +2,8 @@ package com.vibelift.vibelift_mentalhealth_app_new.repository;
 
 import com.vibelift.vibelift_mentalhealth_app_new.model.Doctor;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.Optional;
 
 public interface DoctorRepository extends MongoRepository<Doctor, String> {
+    Optional<Doctor> findByUsername(String username);
 }
